49ffaec10d3de8c59c44284507c3ff94b39f7c24
Kasia <kajzerkatarzyna1@gmail.com>
2023-12-11 16:36:41 +0100

    Created reusable Input.jsx component.

---
+++ src/components/Input.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Input = ({ label, id, ...props}) => {
+    return (
+        <div>
+            <label htmlFor={id}>{label}</label>
+            <input
+                id={id}
+                {...props}
+            />
+        </div>
+    );
+};
+
+export default Input;
